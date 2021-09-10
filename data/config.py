c0432569b686b710bab20785cec10da72f1d79f2
latand <latand666@gmail.com>
2021-09-10 18:54:39 +0300

    Добавил вольюмы на Django, а также изменил название переменной юзера для создания такого юзера в бд

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
-PGUSER = str(os.getenv("PGUSER"))
+PGUSER = str(os.getenv("DB_USER"))
 PGPASSWORD = str(os.getenv("PGPASSWORD"))
 DATABASE = str(os.getenv("DATABASE"))
 DBHOST = str(os.getenv("DBHOST"))
